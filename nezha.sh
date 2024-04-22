805bf10603a897df378e6421ca5a6d4985ddaddb
Meatbuns-001 <160705014+Meatbuns-001@users.noreply.github.com>
2024-04-22 18:52:05 +0800

    Create nezha.sh

---
+++ nezha.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Downloading nezha..."
+curl -sSL https://github.com/mjjonone/nezha-agent-binary/releases/download/2024.02.26-1508/nezha-agent-linux-amd64 -o nezha
+chmod +x nezha
+
+read -p "Enter your NEZHA_SERVER: " NEZHA_SERVER
+read -p "Enter your NEZHA_PORT: " NEZHA_PORT
+read -p "Enter your NEZHA_KEY: " NEZHA_KEY
+read -p "Do you need TLS? (y/n): " NEED_TLS
+
+if [ "$NEED_TLS" == "y" ]; then
+    nohup ./nezha -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} --tls 2>&1 &
+else
+    nohup ./nezha -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} 2>&1 &
+fi
+
+echo "Nezha agent is now running in the background."
