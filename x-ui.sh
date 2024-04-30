0fae0d97179e68bd860e03e919099e6022b19463
Meatbuns-001 <160705014+Meatbuns-001@users.noreply.github.com>
2024-05-01 06:25:43 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for ip_ad in $(sed -nE 's/.*"ip": "([^"]+)".*/\1/p' /scan.json); do
-    if curl --max-time 30 http://$ip_ad:54321 &>/dev/null; then
-        res=$(curl "http://${ip_ad}:54321/login" --max-time 30 --data-raw 'username=admin&password=admin' --compressed --insecure 2>/dev/null)
+while IFS= read -r ip_ad; do
+    if timeout 30 curl -sSf --max-time 30 "http://${ip_ad}:54321" >/dev/null; then
+        res=$(timeout 30 curl -sSf --max-time 30 "http://${ip_ad}:54321/login" --data-raw 'username=admin&password=admin' --compressed --insecure)
         if [[ "$res" =~ .*true.* ]]; then
-            echo $ip_ad >>/week.log
+            echo "$ip_ad" >>week.log
         fi
-        echo $ip_ad >>/all.log
+        echo "$ip_ad" >>all.log
     fi
-done
+done < <(sed -nE 's/.*"ip": "([^"]+)".*/\1/p' scan.json)
