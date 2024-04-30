50039495170a9dce3bf60177040ddca9f16af779
Meatbuns-001 <160705014+Meatbuns-001@users.noreply.github.com>
2024-04-30 21:53:59 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for ip_ad in $(sed -nE  's/.*"ip": "([^"]+)".*/\1/p' scan.json); do
-        if curl --max-time 1 http://$ip_ad:54321; then
-                res=$(curl "http://${ip_ad}:54321/login"  --data-raw 'username=admin&password=admin' --compressed  --insecure)
-                if [[ "$res" =~ .*true.* ]]; then
-                        echo $ip_ad | tee >> week.log
-                fi
-                echo $ip_ad | tee >> all.log
+for ip_ad in $(sed -nE 's/.*"ip": "([^"]+)".*/\1/p' /scan.json); do
+    if curl --max-time 30 http://$ip_ad:54321 &>/dev/null; then
+        res=$(curl "http://${ip_ad}:54321/login" --max-time 30 --data-raw 'username=admin&password=admin' --compressed --insecure 2>/dev/null)
+        if [[ "$res" =~ .*true.* ]]; then
+            echo $ip_ad >>/week.log
         fi
-done;
+        echo $ip_ad >>/all.log
+    fi
+done
